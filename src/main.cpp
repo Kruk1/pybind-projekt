3ad658ac8a1e54bd7da83bc5520bf33e3043e1fa
Albert <albertkr@wp.pl>
2024-05-13 11:49:53 +0200

    Add generate signals (sin, cos, pilo)

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,55 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
-#include <cmath>
+#include <matplot/matplot.h>
+#include <pybind11/stl.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
-#define _USE_MATH_DEFINES
+
+namespace py = pybind11;
+using namespace matplot;
 
 int add(int i, int j) {
     return i + j;
 }
 
-float generate_signal(double frequency, double freq_samp, int samp)
+double generate_signal(double frequency, double freq_samp, int samp, int choose)
 {
-    double faza = fmod(samp * 2 * M_PI * frequency / freq_samp, 2 * M_PI);
-    return faza;
+    double res;
+    double phase = fmod(samp * 2 * M_PI * frequency / freq_samp, 2 * M_PI);
+    if (choose == 1)
+    {
+        res = sin(phase);
+        return res;
+    }
+    else if(choose == 2)
+    {
+        res = cos(phase);
+        return res;
+    }
+    else if (choose == 3)
+    {
+        res = fmod(samp * 2 * frequency / freq_samp + 1, 2) - 1;
+        return res;
+    }
+    else if (choose == 4)
+    {
+        res = cos(phase);
+        return res;
+    }
 }
 
-namespace py = pybind11;
+void show_plot(std::vector<double> x, std::vector<double> y)
+{
+    tiledlayout(1, 1);
+    auto ax1 = nexttile();
+    plot(ax1, x, y);
+    title(ax1, "Wartosc sygnalu");
+
+    show();
+}
 
 PYBIND11_MODULE(_core, m) {
     m.doc() = R"pbdoc(
@@ -31,20 +64,14 @@ PYBIND11_MODULE(_core, m) {
            add
            subtract
     )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+    
+    m.def("generate_signal", &generate_signal, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
     )pbdoc");
-    
-    m.def("generate_signal", &generate_signal, R"pbdoc(
+
+    m.def("show_plot", &show_plot, R"pbdoc(
         Subtract two numbers
 
         Some other explanation about the subtract function.
--- src/scikit_build_example/__init__.py
+++ src/scikit_build_example/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from ._core import __doc__, __version__, add, generate_signal
+from ._core import __doc__, __version__, generate_signal, show_plot
 
-__all__ = ["__doc__", "__version__", "add", "generate_signal"]
+__all__ = ["__doc__", "__version__", "generate_signal", "show_plot"]
