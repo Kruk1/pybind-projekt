9018dab70455e40ee1bd3d14807713bd4d87d64f
Mateusz <m.lachowski24@gmail.com>
2024-05-16 18:15:03 +0200

    viusalization test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,6 +2,16 @@ from __future__ import annotations
 
 import scikit_build_example as m
 
+import wave
+import struct
+def read_wave(filename: str) -> list[int]:
+    with wave.open(filename) as f:
+        data = f.readframes(f.getnframes())
+        samples = struct.unpack('{n}h'.format(n=f.getnframes()*f.getnchannels()), data)
+    return samples
+
+m.visualization(read_wave("tests\\file.wav"))
+
 
 def test_version():
     assert m.__version__ == "0.0.1"
